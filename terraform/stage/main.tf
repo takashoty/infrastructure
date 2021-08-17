63d6783cc762810a8bceda09a856dad85167e1e2
Oleksii Smiichuk <smiychuk@gmail.com>
2021-08-17 12:42:13 +0300

    Rework tf

---
+++ terraform/stage/main.tf
@@ -0,0 +1,10 @@
+provider "google" {
+  credentials = var.service_account_key_file
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
+module "compute_instance" {
+  source = "../modules/"
+}
