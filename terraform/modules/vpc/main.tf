e97a29d08791860851b6c096f69666c208f87b64
Oleksii Smiichuk <smiychuk@gmail.com>
2021-08-24 11:57:19 +0300

    Change name and vars

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,5 +1,18 @@
 resource "google_compute_network" "vpc" {
-//  name = "kv094-name"
-  name                    = var.network_name
-  auto_create_subnetworks = "true"
+  name = "kv094-vpc"
+//  name                    = var.vpc_name
+  auto_create_subnetworks = "false"
 }
+
+//resource "google_compute_network" "vpc" {
+//  name                    = "${var.project_id}-vpc"
+//  auto_create_subnetworks = "false"
+//}
+//
+//# Subnet
+//resource "google_compute_subnetwork" "subnet" {
+//  name          = "${var.project_id}-subnet"
+//  region        = var.region
+//  network       = google_compute_network.vpc.name
+//  ip_cidr_range = "10.10.0.0/24"
+//}
