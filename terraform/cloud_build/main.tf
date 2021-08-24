aa61d2074c9e1c06f999f1ac9e78e560b0eb7aa3
Oleksii Smiichuk <smiychuk@gmail.com>
2021-08-24 23:19:47 +0300

    Change trigger_template to github (don't work)

--- terraform/cloud_build/main.tf
+++ terraform/cloud_build/main.tf
@@ -1,8 +1,15 @@
 resource "google_cloudbuild_trigger" "start_tf_trigger" {
   project = var.project
-  trigger_template {
-    branch_name = "develop-smiichuk"
-    repo_name = "infrastructure"
+//  trigger_template {
+//    branch_name = "develop-smiichuk"
+//    repo_name = "infrastructure"
+//  }
+  github {
+    owner = "devops094"
+    name = "infrastructure"
+    push {
+      branch = "develop-smiichuk"
+    }
   }
 
   filename = "cloudbuild.yaml"
