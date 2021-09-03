54ef75c6c9df37184a0579868b8f09d1cd8414dc
Oleksii Smiichuk <smiychuk@gmail.com>
2021-09-03 17:47:36 +0300

    Some fix in helm chart for Jenkins

--- terraform/modules/helm_charts_jenkins/main.tf
+++ terraform/modules/helm_charts_jenkins/main.tf
@@ -1,14 +1,21 @@
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
+# cheat - if pre-configured k8s with gcloud
+//provider "helm" {
+//  kubernetes {
+//    config_path = "~/.kube/config"
+//  }
+//}
+
+//provider "helm" {
+//  kubernetes {
+//    config_path = "/home/head/PycharmProjects/kv-devops-094/infrastructure/terraform/stage/kubeconfig"
+//  }
+//}
 
 resource "helm_release" "jenkins" {
   chart = "jenkins"
   name = "jenkins"
-  repository = "https://charts.helm.sh/stable"
+  repository = "https://charts.jenkins.io"
 //  namespace = "default"
-//  version = "2.5.4"
+//  version = "3.5.14"
   timeout = 2400
 }
